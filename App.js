d8a181b4bf3fdb72db2e21990de73281ec82b593
TSmith112000 <tsmith112000@gmail.com>
2022-09-29 23:10:25 -0500

    This is the second commit of all time.

--- App.js
+++ App.js
@@ -1,35 +1,7 @@
 import React from 'react';
-import { StyleSheet,View, Text, Image, ScrollView, TextInput } from 'react-native';
+import { StyleSheet, View, Text, Image, ScrollView, TextInput } from 'react-native';
 //import { StatusBar } from 'expo-status-bar';
 //import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-	Test = "Hi,"
-	return (
-		<ScrollView>
-		<Text>Some text</Text>
-		<View>
-			<Text>Some more text</Text>
-				<Image
-					source={{
-					uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-					}}
-					style={{ width: 200, height: 200 }}
-				/>
-			</View>
-			
-			<TextInput
-				style={{
-					height: 50,
-					width: 100,
-					
-					borderColor: 'gray',
-					borderWidth: 1
-				}}
-			/>
-		</ScrollView>
-	);
-}
 
 const styles = StyleSheet.create({
 	container: {
@@ -39,3 +11,23 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 	},
 });
+
+export default function App() {
+	return (
+	<View Style={styles}>
+		<Text> This is a statement </Text>
+		<Image
+			source={require('./Pacman.png')}
+			style=
+			{{
+				width: 40,
+				height: 40,
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}
+		/>
+	</View>
+	);
+}
+
+
